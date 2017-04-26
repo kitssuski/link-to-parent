8a0e0e0dd7cc691665d0c97201b915adb1c50295
kitssuski <kitssuski@gmail.com>
2017-04-25 19:11:10 -0500

    add custom click

--- js/linkToParent.js
+++ js/linkToParent.js
@@ -2,6 +2,26 @@ function linkToParent(i){
 	$(i).each(function(index, el) {
 		$(el).css('cursor', 'pointer').click(function(e) {
 			 $( location ).attr("href", $(el).find('a').attr('href'));
+		}).bind('contextmenu', function(e) {
+			e.preventDefault();
+			customClick(this, e);
 		});
 	});
+}
+function customClick(i, e){
+	// console.log(e);
+	switch(e.button) {
+	  case 1:
+	  console.log('Middle click');
+	  break;
+
+	  case 2:
+ 	  console.log(this);
+	  break;
+
+	  default:
+	  console.log('Click or Left click');
+	  break;
+	}
+
 }
